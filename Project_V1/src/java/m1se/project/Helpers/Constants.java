ada29791216eaa389ce975933f00c636f47bf277
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2019-10-20 13:09:11 +0200

    Modifications des messages d'erreur

--- Project_V1/src/java/m1se/project/Helpers/Constants.java
+++ Project_V1/src/java/m1se/project/Helpers/Constants.java
@@ -12,7 +12,8 @@ package m1se.project.Helpers;
 public class Constants {
     public static final String SEL_QUERY_CREDENTIALS = "SELECT * FROM Credentials";
     public static final String SEL_QUERY_EMPLOYEES = "SELECT * FROM Employees";
-    public static final String ERR_MESSAGE = "Invalid credentials";
+    public static final String ERR_MESSAGE_INVALID= "Connection failed! Verify your login/password and try again.";
+    public static final String ERR_MESSAGE_MISSING= "You must enter values in both fields.";
     public static final String JSP_HOME_PAGE = "WEB-INF/views/home.jsp";
     public static final String JSP_WELCOME_PAGE = "WEB-INF/views/welcome.jsp";
     public static final String FRM_LOGIN_FIELD = "loginSent";
