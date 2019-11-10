2901327b041b90e4c8290e1fea84856d85a8d16b
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2019-11-10 22:55:04 +0100

    Ajout de javadoc

--- Project_V2/src/main/java/m1se/project/Helpers/Constants.java
+++ Project_V2/src/main/java/m1se/project/Helpers/Constants.java
@@ -10,26 +10,80 @@ package m1se.project.Helpers;
  * @author paul-henrizimmerlin
  */
 public class Constants {
-    public static final String SEL_QUERY_CREDENTIALS = "SELECT * FROM Credentials";
-    public static final String SEL_QUERY_EMPLOYEES = "SELECT * FROM Employees";
-    public static final String SEL_QUERY_EMPLOYEE_BY_ID = "SELECT * FROM Employees WHERE ID = ?";
-    public static final String DEL_QUERY_EMPLOYEE_BY_ID = "DELETE FROM Employees WHERE ID = ?";
-    public static final String INS_QUERY_EMPLOYEE = "INSERT INTO Employees (NAME, FIRSTNAME, TELHOME, TELMOB, TELPRO, ADRESS, POSTALCODE, CITY, EMAIL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    public static final String UPD_QUERY_EMPLOYEE_BY_ID = "UPDATE Employees SET NAME = ?, FIRSTNAME = ?, TELHOME = ?, TELMOB = ?, TELPRO = ?, ADRESS = ?, POSTALCODE = ?, CITY = ?, EMAIL = ? WHERE ID = ?";
+
+    /**
+     * The message to display if no matching result for combinaison user/password is found in the database
+     */
     public static final String ERR_MESSAGE_INVALID= "Connection failed! Verify your login/password and try again.";
+
+    /**
+     * The message to display if one or more field is empty
+     */
     public static final String ERR_MESSAGE_MISSING= "You must enter values in both fields.";
+
+    /**
+     * The message to display if the employee is not found in the database
+     */
     public static final String ERR_MESSAGE_NOT_FOUND = "The selected employee was not found in the database.";
+
+    /**
+     * Path to home (login) page
+     */
     public static final String JSP_HOME_PAGE = "WEB-INF/views/home.jsp";
+
+    /**
+     * Path to welcome (list of all employess) page
+     */
     public static final String JSP_WELCOME_PAGE = "WEB-INF/views/welcome.jsp";
+
+    /**
+     * Path to details (add or update an employee) page 
+     */
     public static final String JSP_DETAIL_PAGE = "WEB-INF/views/details.jsp";
+
+    /**
+     * Path to exit (logout) page
+     */
     public static final String JSP_EXIT_PAGE = "WEB-INF/views/exit.jsp";
+
+    /**
+     * Name of the login field in the home page
+     */
     public static final String FRM_LOGIN_FIELD = "loginSent";
+
+    /**
+     * Name of the password field in the home page
+     */
     public static final String FRM_PWD_FIELD = "passwordSent";
     
+    /**
+     * Name of the id field
+     */
+    public static final String FRM_ID_FIELD = "id";
+
+    /**
+     * Name of the name field
+     */
     public static final String FRM_NAME_FIELD = "name";
+
+    /**
+     * Name of the first name field
+     */
     public static final String FRM_FIRSTNAME_FIELD = "firstname";
+
+    /**
+     * Name of the professionnal phone number
+     */
     public static final String FRM_TELPRO_FIELD = "telpro";
+
+    /**
+     * Name of the mobile phone number
+     */
     public static final String FRM_TELMOB_FIELD = "telmob";
+
+    /**
+     * Name of the home phone number
+     */
     public static final String FRM_TELDOM_FIELD = "teldom";
     public static final String FRM_ADDRESS_FIELD = "address";
     public static final String FRM_CITY_FIELD = "city";
@@ -37,7 +91,6 @@ public class Constants {
     public static final String FRM_EMAIL_FIELD = "email";
     
     public static final String FRM_ACTION_LOGIN = "login";
-    public static final String FRM_ID_FIELD = "id";
     public static final String FRM_ACTION_DELETE = "delete";
     public static final String FRM_ACTION_DETAIL = "details";
     public static final String FRM_ACTION_ADD = "add";
