f3b8a203ce0e3a4e7c623babd477db93614f9310
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2019-11-12 08:46:21 +0100

    Ajout de la javadoc

--- Project_V1/src/java/m1se/project/Model/User.java
+++ Project_V1/src/java/m1se/project/Model/User.java
@@ -14,26 +14,50 @@ public class User {
     private String password;
     private Boolean isAdmin;
 
+    /**
+     * Get the login of an user
+     * @return The login of the user
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * Set the login of an user
+     * @param login
+     */
     public void setLogin(String login) {
         this.login = login;
     }
 
+    /**
+     * Get the password of an user
+     * @return The password of the user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Set the password of an user
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Get the status of an user (true if admin)
+     * @return The status of the user
+     */
     public Boolean getIsAdmin() {
         return isAdmin;
     }
 
+    /**
+     * Set the status of an user
+     * @param isAdmin
+     */
     public void setIsAdmin(Boolean isAdmin) {
         this.isAdmin = isAdmin;
     }
